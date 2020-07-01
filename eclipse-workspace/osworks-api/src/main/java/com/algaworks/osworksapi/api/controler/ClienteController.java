966fb86305382636b6d3c8d3000b3d99b8a52c05
Hipertrofia <jansen@dcc.ufrj.br>
2020-07-01 14:42:11 -0300

    Adicionando os métodos post e put ao client controller

--- eclipse-workspace/osworks-api/src/main/java/com/algaworks/osworksapi/api/controler/ClienteController.java
+++ eclipse-workspace/osworks-api/src/main/java/com/algaworks/osworksapi/api/controler/ClienteController.java
@@ -4,9 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworksapi.domain.model.Cliente;
@@ -34,5 +40,23 @@ public class ClienteController {
 		
 		return ResponseEntity.notFound().build(); 
 	}
-
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar (@RequestBody Cliente cliente) {
+		return clientRepository.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente){
+		
+		if(!clientRepository.existsById(clienteId)){
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clientRepository.save(cliente);
+		return ResponseEntity.ok(cliente);
+	}
 }
